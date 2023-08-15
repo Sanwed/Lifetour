49357815046ed2c60fea8f8377956e79a1ee97f3
Sanwed <kozyuckov.alex@gmail.com>
2023-08-15 17:25:51 +0400

    Туры

--- source/js/modules/sliders/sliders.js
+++ source/js/modules/sliders/sliders.js
@@ -6,13 +6,38 @@ const initSliders = () => {
     loop: true,
     speed: 300,
     watchOverflow: true,
-    allowTouchMove: false,
     slideClass: 'hero__slider-slide',
     pagination: {
       el: '.hero__slider-pagination',
       clickable: true,
       bulletClass: 'hero__slider-bullet',
       bulletActiveClass: 'hero__slider-bullet--active',
+    },
+    breakpoints: {
+      1200: {
+        allowTouchMove: false,
+      },
+    },
+  });
+
+  const toursSlider = new Swiper('.tours__slider', {
+    loading: 'lazy',
+    watchOverflow: true,
+    slideClass: 'tours__slide',
+    navigation: {
+      nextEl: '.tours__slider-arrow--next',
+      prevEl: '.tours__slider-arrow--prev',
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 18,
+      },
+      1200: {
+        allowTouchMove: false,
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
     },
   });
 };
