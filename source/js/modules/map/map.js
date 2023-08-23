13c943de146411db68b1ad516ef3224155145918
Sanwed <kozyuckov.alex@gmail.com>
2023-08-23 13:58:33 +0400

    Карта

---
+++ source/js/modules/map/map.js
@@ -0,0 +1,19 @@
+const initMap = () => {
+  const map = L
+    .map('map', {
+      scrollWheelZoom: false,
+    })
+    .setView([55.81656, 37.63772], 16);
+
+  const layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+  map.addLayer(layer);
+
+  const markerIcon = L.icon({
+    iconUrl: '../img/svg/pin-filled.svg',
+    iconSize: [48, 48],
+  });
+  const marker = new L.Marker([55.81656, 37.63772], {icon: markerIcon});
+  marker.addTo(map);
+};
+
+export {initMap};
